0d1b70efefe7530bbf1c6cbabbb5e630659d07f9
RikaiyahWinters <rikaiyahswinters@gmail.com>
2021-05-28 22:06:47 -0500

    Corrected repo href url

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ const displayRepoInfo = async function(repoInfo, languages) {
     <p>Description: ${repoInfo.description}</p>
     <p>Default Branch: ${repoInfo.default_branch}</p>
     <p>Languages: ${languages.join(", ")}</p>
-    <a class="visit" href="${repoInfo.url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
   `;
   repoData.append(div);
   repoData.classList.remove("hide");
